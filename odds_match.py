16e09bde071f51a821561c60c2aeb1f51ce8870d
DamienQua <dam.qua@hotmail.fr>
2024-03-25 15:31:54 +0100

    right way to requests on the other player

--- odds_match.py
+++ odds_match.py
@@ -1,13 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-from indice_confiance import indice_confiance
 
 def odds_match(match_vs, indice_tab) :
     session = HTMLSession()
-    resp = session.get("https://www.oddsportal.com/search/" + match_vs[2])
+    resp = session.get("https://www.oddsportal.com/search/" + match_vs[2].split(" ")[-1])
     resp.html.render()
     driver = BeautifulSoup(resp.html.html, "lxml")
     try :
@@ -41,7 +37,9 @@ def odds_match(match_vs, indice_tab) :
             match_vs[38] = odds[1]
             match_vs[39] = odds[0]
     except :
-        driver.get("https://www.oddsportal.com/search/" + match_vs[3])
+        resp = session.get("https://www.oddsportal.com/search/" + match_vs[3].split(" ")[-1])
+        resp.html.render()
+        driver = BeautifulSoup(resp.html.html, "lxml")
         try :
             match = driver.find_all(class_="flex w-full items-center") # relative w-full flex-col flex text-xs leading-[16px] min-w-[0] gap-1 next-m:!flex-row next-m:!gap-2 justify-center
             odds = [] 
@@ -84,40 +82,3 @@ def odds_match(match_vs, indice_tab) :
         indice_tab[23] = 20
 
     session.close()
-
-    ''' url = "oddsportal.com/search/"
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://www.oddsportal.com/search/" + match_vs[2])
-    time.sleep(0.1)
-    try :
-        match = driver.find_elements_by_class_name("table-participant")
-        i = 0
-        while "/" in match[i].text :
-            i += 1
-        match = match[i].text.strip()
-        if match.find(match_vs[2].split(" ")[-1]) < match.find(match_vs[3].split(" ")[-1]) :
-            match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i].text))
-            match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i+1].text))
-        else :
-            match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i+1].text))
-            match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i].text))
-    except ValueError :
-        driver.get("https://www.oddsportal.com/search/" + match_vs[3])
-        try :
-            match = driver.find_element_by_class_name("table-participant").text.strip()
-            i = 0
-            while "/" in match[i].text :
-                i += 1
-            match = match[i].text.strip()
-            if match.find(match_vs[2].split(" ")[-1]) < match.find(match_vs[3].split(" ")[-1]) :
-                match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i].text))
-                match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i+1].text))
-            else :
-                match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i+1].text))
-                match_vs.append(float(driver.find_elements_by_class_name("odds-nowrp")[2*i].text))
-        except ValueError :
-                match_vs.append(0)
-                match_vs.append(0)
-    driver.close() '''
