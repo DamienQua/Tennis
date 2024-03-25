236396505a3748a9d1106ec22f41eb708605bd44
DamienQua <dam.qua@hotmail.fr>
2024-03-25 15:47:09 +0100

    add definition of tie_break variable

--- indice_confiance.py
+++ indice_confiance.py
@@ -82,6 +82,7 @@ def indice_confiance(indice_tab, session, admin_url, header, player_id, match_id
             points_save_bp = float(stats_match.find_all(class_="col-xs-4")[18+j2].text.strip().split("%")[0])
             return_save_bp = float(stats_match.find_all(class_="col-xs-4")[30+j2].text.strip().split("%")[0])
             scores = re.sub(r"\([^)]*\)", "", stats_match.find_all(class_="col-xs-12")[0].find_all("h4")[0].text.strip())
+            tie_break_j1, tie_break_j2 = 0, 0
             if scores != "" :
                 tie_break_j1 = sum(int(s[0]) >= 6 and int(s[1]) >= 6 and int(s[0]) > int(s[1]) for score in scores.split(" ") for s in (score.split("-"),))
                 tie_break_j2 = sum(int(s[0]) >= 6 and int(s[1]) >= 6 and int(s[0]) < int(s[1]) for score in scores.split(" ") for s in (score.split("-"),))
